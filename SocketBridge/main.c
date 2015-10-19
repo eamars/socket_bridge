ed53d0ecfacf8bceaaa93cf22289100eec2586e1
Ran Bao <eamars@rans-imac.local>
2015-10-19 14:44:10 +1300

    Initial Commit

---
+++ SocketBridge/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SocketBridge
+//
+//  Created by Ran Bao on 19/10/15.
+//  Copyright © 2015 Ran Bao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
